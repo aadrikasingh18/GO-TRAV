125944e29ff1c116d69ca45e3e9edba28368b944
aadrikasingh18 <aadrikas16@gmail.com>
2024-08-23 22:24:09 +0530

    Modification

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Blogs from './components/pages/Blogs';
-import Places from './components/pages/Places';
-import SignUp from './components/pages/SignUp';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/blogs' component={Blogs} />
-          <Route path='/places' component={Places} />
-          <Route path='/' component={SignUp} />
-        </Switch>
-      </Router>
-    </>
-  );
+    <div>
+      HELLO WORLD
+    </div>
+  )
 }
 
-export default App;
+export default App
