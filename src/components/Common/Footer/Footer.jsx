165f426a4689a92d1aa4e0cf00843a3458f6161e
aadrikasingh18 <aadrikas16@gmail.com>
2024-08-24 01:08:18 +0530

    Footer Modified

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -1,118 +1,125 @@
+// toggleVisible Function: This function checks the scroll position of the document. If the user scrolls down more than 300 pixels, it sets visible to true, showing the "back to top" button. Otherwise, it hides the button.
+
+// scrollTop Function: When called, it smoothly scrolls the page back to the top.
+
 import React, { useState } from "react";
 import "../Footer/footer.css"
-import { Col, Container, Row ,ListGroup} from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import {Container} from "react-bootstrap";
 
 const Footer = () => {
-  const [visible, setVisible]=useState(false);
+  const [visible, setVisible] = useState(false);
 
-  const toggleVisible=()=>{
+  const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
-    if(scrolled > 300){
+    if (scrolled > 300) {
       setVisible(true)
     }
-   else if(scrolled  <= 300){
+    else if (scrolled <= 300) {
       setVisible(false)
     }
   }
 
-  const scrollTop =()=>{
+  const scrollTop = () => {
     window.scrollTo({
-      top:0,
-      behavior:"smooth"
+      top: 0,
+      behavior: "smooth"
     })
   }
 
-  if(typeof window !== "undefined"){
+  if (typeof window !== "undefined") {
     window.addEventListener("scroll", toggleVisible)
   }
 
-
   return (
     <>
-    <footer className="pt-5">
-      <Container>
-        <Row>
-          <Col md="3" sm="12" className="quick_link mt-3 mt-md-0 ">
-            <h4 className="mt-lg-0 mt-sm-3">Company </h4>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <NavLink to="/">About Us</NavLink>
-              </ListGroup.Item>
-              <ListGroup.Item>
-              <NavLink to="/">News</NavLink>
-              </ListGroup.Item>
-              <ListGroup.Item>
-              <NavLink to="/">Faq</NavLink>
-              </ListGroup.Item>
-             
-            </ListGroup>
-          </Col>
-          <Col md="3" sm="12" className="quick_link mt-3 mt-md-0 ">
-          <h4 className="mt-lg-0 mt-sm-3">Explore </h4>
-          <ListGroup variant="flush">
-              <ListGroup.Item>
-                <NavLink to="/"> Faq</NavLink>
-              </ListGroup.Item>
-              <ListGroup.Item>
-              <NavLink to="/">Tour Listings</NavLink>
-              </ListGroup.Item>
-              <ListGroup.Item>
-              <NavLink to="/"> Destination</NavLink>
-              </ListGroup.Item>
-             
-            </ListGroup>
-          </Col>
-          <Col md="3" sm="12" className="quick_link mt-3 mt-md-0 ">
-          <h4 className="mt-lg-0 mt-sm-3">Quick Link </h4>
-          <ListGroup variant="flush">
-              <ListGroup.Item>
-                <NavLink to="/"> Home</NavLink>
-              </ListGroup.Item>
-              <ListGroup.Item>
-              <NavLink to="/">About Us </NavLink>
-              </ListGroup.Item>
-              <ListGroup.Item>
-              <NavLink to="/"> Contact Us </NavLink>
-              </ListGroup.Item>
-             
-            </ListGroup>
-          </Col>
-          <Col md="3" sm="12" className="location mt-3 mt-md-0 ">
-          <h4 className="mt-lg-0 mt-sm-3">Contact Info </h4>
+      <footer className="p-2">
+        <Container>
+        <p className="text-light dancing-script-travel">To Travel is to Live </p>
+        <p className="text-light"> Copyright @ 2024 </p>
+        </Container>
+      </footer>
 
-          <div className="d-flex align-items-center">
-            <p className="pb-2"> Dehradun, Uttarakhand, India</p>
-          </div>
-
-          <div className="d-flex align-items-top my-2">
-          <i className="bi bi-geo-alt me-3"></i>
-          <a target="_blank" href="mailto:rawatcoder@gmail.com" className="d-block" >rawatcoder@gmail.com</a>
-          </div>
-          <div className="d-flex align-items-top ">
-          <i className="bi bi-telephone me-3"></i>
-          <a target="_blank" href="tel:9876543210" className="d-block" >9876543210</a>
-          </div>
-         
-          </Col>
-        </Row>
-        <Row className="py-2 bdr mt-3">
-        <Col className="col copyright">
-        <p className="text-light">   @ 2024. rawatcoder All rights reserved </p> 
-        </Col>
-        </Row>
-
-      </Container>
-    </footer>
-
-    <div id="back-top"
-    onClick={scrollTop}
-     className={visible ? "active" : ""}>
-    <i className="bi bi-arrow-up"></i>
-
-    </div>
+      <div id="back-top"
+        onClick={scrollTop}
+        className={visible ? "active" : ""}>
+        <i className="bi bi-arrow-up"></i>
+      </div>
     </>
   );
 };
 
 export default Footer;
+
+
+{/*   
+  <footer className="pt-5">
+    <Container>
+      <Row>
+        <Col md="3" sm="12" className="quick_link mt-3 mt-md-0 ">
+          <h4 className="mt-lg-0 mt-sm-3">Company </h4>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <NavLink to="/">About Us</NavLink>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <NavLink to="/">News</NavLink>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <NavLink to="/">Faq</NavLink>
+              </ListGroup.Item>
+            </ListGroup>
+        </Col>
+        <Col md="3" sm="12" className="quick_link mt-3 mt-md-0 ">
+          <h4 className="mt-lg-0 mt-sm-3">Explore </h4>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <NavLink to="/"> Faq</NavLink>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <NavLink to="/">Tour Listings</NavLink>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <NavLink to="/"> Destination</NavLink>
+              </ListGroup.Item>
+            </ListGroup>
+        </Col>
+        <Col md="3" sm="12" className="quick_link mt-3 mt-md-0 ">
+            <h4 className="mt-lg-0 mt-sm-3">Quick Link </h4>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <NavLink to="/"> Home</NavLink>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <NavLink to="/">About Us </NavLink>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <NavLink to="/"> Contact Us </NavLink>
+              </ListGroup.Item>
+            </ListGroup>
+        </Col>
+            <Col md="3" sm="12" className="location mt-3 mt-md-0 ">
+              <h4 className="mt-lg-0 mt-sm-3">Contact Info </h4>
+
+              <div className="d-flex align-items-center">
+                <p className="pb-2"> Dehradun, Uttarakhand, India</p>
+              </div>
+
+              <div className="d-flex align-items-top my-2">
+                <i className="bi bi-geo-alt me-3"></i>
+                <a target="_blank" href="mailto:rawatcoder@gmail.com" className="d-block" >rawatcoder@gmail.com</a>
+              </div>
+              <div className="d-flex align-items-top ">
+                <i className="bi bi-telephone me-3"></i>
+                <a target="_blank" href="tel:9876543210" className="d-block" >9876543210</a>
+              </div>
+
+            </Col>
+          </Row>
+          <Row className="py-2 bdr mt-3">
+            <Col className="col copyright">
+              <p className="text-light">   @ 2024. rawatcoder All rights reserved </p>
+            </Col>
+          </Row>
+        </Container>
+      </footer> 
+*/}
