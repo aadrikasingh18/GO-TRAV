9dbc5e234819c5536f4a3e5552cf3e462beb3893
aadrikasingh18 <aadrikas16@gmail.com>
2024-08-25 00:44:39 +0530

    Features Updated

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../Features/features.css";
 
-import feature1 from "../../assets/images/feature/beach-umbrella.png";
-import feature2 from "../../assets/images/feature/deal.png";
-import feature3 from "../../assets/images/feature/location.png";
-import feature4 from "../../assets/images/feature/medal.png";
+import feature1 from "../../assets/images/feature/adventurer.png";
+import feature2 from "../../assets/images/feature/hot-deal.png";
+import feature3 from "../../assets/images/feature/art-show.png";
+import feature4 from "../../assets/images/feature/trust.png";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -65,27 +65,27 @@ const Features = () => {
     {
       id: 0,
       image: feature1,
-      title: "Discover the possibilities",
-      des: "With nearly half a million attractions, hotels & more, you're sure to find joy.",
+      title: "Explore the Possibilities",
+      des: "With nearly half a million options, including attractions, hotels, and more, you're bound to discover something that brings you joy. We do the hard work so you don’t have to.",
     },
     {
       id: 1,
       image: feature2,
-      title: "Enjoy deals & delights",
-      des: "Quality activities. Great prices. Plus, earn credits to save more.",
+      title: "Hot Deals and Offer",
+      des: "Along with quality, you also enjoy the lowest prices, last-minute availability, and 24/7 support.",
     },
     {
       id: 2,
       image: feature3,
-      title: "Exploring made easyt",
-      des: "Book last minute, skip lines &amp; get free cancellation for easier exploring.",
+      title: "Savor Every Flavor",
+      des: "Whether you're into the unconventional or the popular, a guided tour or a live show, a thrilling game or a museum visit – we've got it all.",
     },
 
     {
       id: 3,
       image: feature4,
-      title: "Travel you can trust",
-      des: "Read reviews & get reliable customer support. We're with you at every step.",
+      title: "Trusted Travel Experiences",
+      des: "Browse reviews and count on reliable customer support. We’re with you at every step, ensuring your journey is enjoyable, and worry-free, from start to end.",
     },
   ];
 
